42677c6bd5d66e769d49a8ee135f5ec21d6e8db8
Evan Buss <evan.buss28@gmail.com>
2019-11-16 22:36:29 -0500

    Center text in table rows

--- web/src/components/Library/MetadataItem.svelte
+++ web/src/components/Library/MetadataItem.svelte
@@ -8,13 +8,13 @@
 </script>
 
 <div
-  class="p-2 w-full flex {alternate ? 'bg-gray-200 hover:bg-blue-100' : 'bg-white hover:bg-blue-100'}">
+  class="p-2 w-full flex justify-center items-center {alternate ? 'bg-gray-200 hover:bg-blue-100' : 'bg-white hover:bg-blue-100'}">
   <a href={movie.url} target="_blank" rel="noopener" class="w-2/12 truncate">
     {movie.title}
   </a>
-  <div class="w-7/12 truncate">{movie.summary}</div>
-  <div class="w-1/12">{movie.year}</div>
-  <div class="w-1/12 text-center">{movie.rating}</div>
+  <span class="w-7/12 truncate">{movie.summary}</span>
+  <span class="w-1/12">{movie.year}</span>
+  <span class="w-1/12 text-center">{movie.rating}</span>
   <button
     on:click={() => dispatch('update', movie.id)}
     class="w-1/12 p-1 bg-blue-400 hover:bg-blue-300 rounded-sm text-white">
