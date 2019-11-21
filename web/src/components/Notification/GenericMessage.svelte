07c002c948db326b166ac62b34b35cdc9b8f173c
Evan Buss <evan.buss28@gmail.com>
2019-11-21 14:56:54 -0500

    update message component

--- web/src/components/Notification/GenericMessage.svelte
+++ web/src/components/Notification/GenericMessage.svelte
@@ -4,13 +4,13 @@
   export let notification;
 </script>
 
-<div class="rounded shadow w-full max-w-md bg-white p-2 mb-2">
+<div class="relative rounded shadow w-full max-w-md bg-white p-2 mb-2">
   <div class="flex flex-row w-full justify-between">
     <h1 class="font-bold text-blue-600 truncate">{notification.title}</h1>
     <i
       on:click={() => dispatch('close')}
-      class="la la-times-circle text-2xl text-red-500 hover:text-red-400
-      cursor-pointer" />
+      class="absolute right-0 top-0 mt-1 mr-1 la la-times-circle text-2xl
+    text-red-500 hover:text-red-300 cursor-pointer" />
   </div>
   <span class="text-gray-600 break-words">{notification.message}</span>
 </div>
